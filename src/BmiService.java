1774c6cf9ef26895e7f22dc0d1c28d68a2e0e51d
Chingis <chingisbur@gmail.com>
2023-10-10 07:49:22 +0300

    Initial commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate(int WeightInKg, double HeightInMeters){
-        double Bmi = (WeightInKg/HeightInMeters)/ HeightInMeters;
+    public int calculate(int WeightInKg, double HeightInMeters) {
+        double Bmi = (WeightInKg / HeightInMeters) / HeightInMeters;
         return (int) Bmi;
 
     }
